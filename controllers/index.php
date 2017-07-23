0ab001ca12be59913fd49f1192a5cc6896856915
root <root@localhost.localdomain>
2017-07-23 15:35:08 +0000

    get match by day

--- controllers/index.php
+++ controllers/index.php
@@ -13,7 +13,44 @@ class Index extends Controller{
     }
 
     public function getMatchOfDay(){
-        $this->model->getMatchOfDay();
+        if(isset($_GET['day'])){
+            $day = $this->protect($_GET['day']);
+            if($day == 'today'){
+                $m_time = time();
+            }else if($day == 'yesterday'){
+                $m_time = time() - 24 * 60 * 60;
+            }else if($day == 'tomorrow'){
+                $m_time = time() + 24 * 60 * 60;
+            }else{
+                $m_time = false;
+            }
+
+            $matches = $this->model->getMatchOfDay();
+            $final_matches = $this->getMatchesByDay($matches, $m_time);
+            echo json_encode($final_matches);
+        }
+
+    }
+
+
+    // get matchs by day
+    public function getMatchesByDay($matches, $m_time = false){
+        $final_matches = [];
+        $m_date = date('Y-m-d', $m_time);
+        if($m_time != false){
+            foreach($matches as $m){
+                $game_date_in_seconds = strtotime($m->mat_time);
+                $game_date = date('Y-m-d', $game_date_in_seconds);
+                if($game_date == $m_date){
+                    $final_matches[] = $m;
+                }else continue;
+            }
+            // echo var_dump($final_matches);
+            return $final_matches;
+        }else{
+            // echo var_dump($matches);
+            return $matches;
+        }
     }
 
 }
--- models/index_model.php
+++ models/index_model.php
@@ -8,9 +8,9 @@ class Index_Model extends Model{
     
     public function getMatchOfDay(){
         $matches = $this->db
-                 ->table('urls INNER JOIN matches on matches.mat_id = urls.url_game inner join channels on channels.chan_id = urls.url_channel inner join commentor on commentor.comm_chan = channels.chan_id inner join champs on champs.champ_id = matches.mat_champ')
+                 ->table('urls INNER JOIN matches on matches.mat_id = urls.url_game inner join channels on channels.chan_id = urls.url_channel inner join commentor on commentor.comm_chan = channels.chan_id inner join champs on champs.champ_id = matches.mat_champ order by matches.mat_time asc')
                  ->select("channels.*, matches.*, commentor.*, champs.*, urls.*");
-        echo var_dump($matches);
+        return $matches;
     }
 }
 ?>
